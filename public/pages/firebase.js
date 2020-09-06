2ccc39a784c306fd99fff60340a99f04ea3f6b9b
Roi Madmon <roipk123@gmail.com>
2020-09-06 10:59:39 +0300

    add firebase to project

---
+++ public/pages/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD91v4LsW6wytMO6tJoUE7xyVz6BLTm5jk",
+    authDomain: "ybl-project-b5e04.firebaseapp.com",
+    databaseURL: "https://ybl-project-b5e04.firebaseio.com",
+    projectId: "ybl-project-b5e04",
+    storageBucket: "ybl-project-b5e04.appspot.com",
+    messagingSenderId: "288673703911",
+    appId: "1:288673703911:web:e0cc3b8ea3fc61019c3f42",
+    measurementId: "G-1RWZH22PZ6"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
