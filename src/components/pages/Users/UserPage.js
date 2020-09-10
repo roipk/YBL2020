b63cc3ac17c88d5a71b7ef0ba548e4ab22b399fc
hadar <cohen.hdr@gmail.com>
2020-09-10 16:10:19 +0300

    loading spinner buttons

--- src/components/pages/Users/UserPage.js
+++ src/components/pages/Users/UserPage.js
@@ -5,6 +5,18 @@ import { Link } from "react-router-dom";
 import { withRouter} from 'react-router'
 import User from './student/Users'
 
+// import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
+
+
+
+
+// const override = css`
+//   display: block;
+//   margin: 0 auto;
+//   border-color: red;
+// `;
+
 class UserPage extends React.Component {
     constructor(props) {
         super(props);
@@ -12,11 +24,13 @@ class UserPage extends React.Component {
             isLoad:false,
             user:{},
             error:false,
-
+            loading: true,
         };
+    }
 
-
-
+    loadPage(event){
+        this.setState({loading:event})
+        //    this.render()
     }
 
     async componentDidMount() {
@@ -45,8 +59,10 @@ class UserPage extends React.Component {
 
 
     async  logout() {
-        auth.signOut();
+        //מסך טעינה
+        await auth.signOut();
         window.location.href = '/';
+        //סיום מסך טעינה
     }
 
     render() {
@@ -68,6 +84,23 @@ class UserPage extends React.Component {
                         state: this.state }}>
                         <button className="btn btn-info">go to new user</button>
                     </Link>
+
+                    <button onClick={() => this.loadPage(true)}>loading page</button>
+                    <button onClick={() => this.loadPage(false)}>unloading page</button>
+
+    {!this.state.loading? "":
+                    <div  className="sweet-loading" >
+        <ClipLoader style={{
+                backgroundColor: "rgba(255,255,255,0.85)",
+                borderRadius: "25px"}}
+        //   css={override}
+          size={150}
+          color={"#123abc"}
+        
+        />
+      </div>
+                }
+                    
                 </div>
             );
         }
