20f9013581143daecb5015e9e8ef39795e32dbdd
Roi Madmon <roipk123@gmail.com>
2020-10-08 23:10:28 +0300

    update teams on manager

--- src/components/pages/Users/Manager/UpdatesFirebase.js
+++ src/components/pages/Users/Manager/UpdatesFirebase.js
@@ -3,19 +3,22 @@ import {db,CreateNewTeam} from "../../../../firebase/firebase";
 import Grid from "@material-ui/core/Grid";
 import TempManager from "./TempManager";
 import {BackPage} from "../UserPage";
+import Select from "react-select";
 var options = []
 class UpdatesFirebase extends Component {
 
-    constructor() {
-        super();
-
+    constructor(props) {
+        super(props);
 
         this.state =
             {
                 isLoaded:false,
                 date:"",
+                newTeamName:'',
                 teamPath:"",
                 teamName:"",
+                replaceTeamName:false
+
             }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChangeDate = this.handleChangeDate.bind(this)
@@ -26,24 +29,74 @@ class UpdatesFirebase extends Component {
     {
 
         var value = event.target.value;
-        this.setState({teamName:value})
+        if(value === '')
+            this.setState({newTeamName:value,replaceTeamName:false})
+        else
+            this.setState({newTeamName:value})
     }
 
 
     render() {
-        console.log(this.state.date)
-        if(this.state.page ==='menu')
-            return(
-                <TempManager>
+        return(
+            <div id="instactorReport" className="sec-design" dir='rtl'>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <input type="text" name="team" placeholder="שם קבוצה חדשה" onChange={this.handleChange}/>
+                    </Grid>
+                    <Grid item xs={6} >
+                        <button onClick={()=>{
+                            CreateNewTeam(this.state.newTeamName)
+                            alert("הקבוצה נוספה בהצלחה")
+                            window.location.reload(true);}}>צור קבוצה חדשה</button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <button onClick={()=>{
+                            if(this.state.newTeamName && this.state.newTeamName.length > 0)
+                                this.setState({replaceTeamName:true})
+                            else
+                                alert("שם הקבוצה החדשה לא יכול להיות ריק")
+                        }}>החלף שם לקבוצה קיימת </button>
+                    </Grid>
+                    <Grid item xs={8} hidden={!this.state.replaceTeamName}>
+                        <Select  placeholder={" בחר קבוצה להחלפת שם "} options={options} onChange={(e)=>{
+                            console.log(e.label,e.value);
+                            this.setState({teamPath:e.value,teamName:e.label})
+                        }} required/>
 
-                </TempManager>
-            )
+                    </Grid>
+                    <Grid item xs={4} hidden={!this.state.replaceTeamName} >
+                        <button onClick={async ()=>{
 
-        else
-            return(this.attendReport())
+                            if(this.state.teamName !== this.state.newTeamName) {
+                                await this.setState({replaceTeamName: false})
+                                this.state.teamPath.update({
+                                    name:this.state.newTeamName
+                                })
+                                alert('בוצע שינוי שם  קבוצה בהצלחה')
+                                window.location.reload(true);
 
+                            }
+                            else
+                            {
+                                console.log("שם זהה לא ניתן לשנות")
+                            }
+                        }}>אישור החלפה</button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <div className="text-below-image">
 
+                            <button onClick={this.handleSubmit} >רשימת מדריכים</button>
+                            <div></div>
+                            <button id="feedback-button" className="btn btn-info" onClick={()=>{BackPage(this.props,this.state.user)}}>חזרה לתפריט</button>
+                        </div>
+
+                    </Grid>
+                </Grid>
+            </div>
+
+        )
     }
+
     async handleSubmit(event)
     {
         console.log(this.state.teamPath)
@@ -74,14 +127,35 @@ class UpdatesFirebase extends Component {
     attendReport() {
 
         return(
-            <div id="instactorReport" className="sec-design">
+            <div id="instactorReport" className="sec-design" dir='rtl'>
                 <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <button onClick={()=>{CreateNewTeam(this.state.teamName)}}>צור קבוצה</button>
+                    <Grid item xs={12}>
+                        <input type="text" name="team" placeholder="שם קבוצה חדשה" onChange={this.handleChange}/>
                     </Grid>
-                    <Grid item xs={8}>
-                            <input type="text" name="team" placeholder="שם קבוצה חדשה" onChange={this.handleChange}/>
-                        </Grid>
+                    <Grid item xs={6} >
+                        <button onClick={()=>{CreateNewTeam(this.state.teamName)}}>צור קבוצה חדשה</button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <button onClick={()=>{
+                        if(this.state.teamName && this.state.teamName.length > 0)
+                            this.setState({replaceTeamName:true})
+                        else
+                            alert("שם הקבוצה החדשה לא יכול להיות ריק")
+                        }}>החלף שם לקבוצה קיימת </button>
+                    </Grid>
+                    <Grid item xs={8} hidden={!this.state.replaceTeamName}>
+                    <Select  placeholder={" בחר קבוצה להחלפת שם "} options={options} onChange={(e)=>{
+                            console.log(e.label,e.value);
+                            this.setState({teamPath:e.value})
+                        }} required/>
+
+                    </Grid>
+                    <Grid item xs={4} hidden={!this.state.replaceTeamName} >
+                        <button onClick={()=>{
+                            console.log("בוצעה החלפה")
+                            this.setState({replaceTeamName:false})
+                        }}>אישור החלפה</button>
+                    </Grid>
                         <Grid item xs={12}>
                             <div className="text-below-image">
 
