d059144ad0c6ff59b8756da0239f4c6fb4876bcb
Roi Madmon <roipk123@gmail.com>
2020-10-15 01:17:40 +0300

    guide and student list in manager

--- src/components/pages/Users/Manager/UpdatesFirebase.js
+++ src/components/pages/Users/Manager/UpdatesFirebase.js
@@ -16,7 +16,10 @@ class UpdatesFirebase extends Component {
                 newTeamName:'',
                 teamPath:"",
                 teamName:"",
-                replaceTeamName:false
+                replaceTeamName:false,
+                delete:false,
+                showGuides:false,
+                showStudents:false
 
             }
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -85,9 +88,9 @@ class UpdatesFirebase extends Component {
                     </Grid>
                     <Grid item xs={12}>
                         <button onClick={()=>{
-                            this.setState({delete:true})
+                            this.setState({delete:!this.state.delete})
                             this.deleteTeam()
-                        }}>מחק קבוצה </button>
+                        }}>{this.state.delete?'הסתר מחיקת קבוצה':'הצג מחיקת קבוצה'} </button>
                     </Grid>
                     <Grid item xs={8} hidden={!this.state.delete}>
                         <Select  placeholder={" בחר קבוצה "} options={options} onChange={(e)=>{
@@ -116,18 +119,83 @@ class UpdatesFirebase extends Component {
                     </Grid>
                     <Grid item xs={12}>
                         <div className="text-below-image">
+                            <button onClick={()=>{
+                                this.getAllUsers('guides')
+                                this.setState({showGuides:!this.state.showGuides})
+                            }} >{this.state.showGuides?'הסתר רשימת מדריכים':'הצג רשימת מדריכים'}</button>
+                            {
+                                (this.state.showGuides && this.state.Guides )?(<div> נמצאו: {this.state.Guides.length} מדריכים </div>):(<div></div>)
+                            }
+                            {
+                                (!this.state.Guides || !this.state.showGuides)?<div></div>:
+                                    this.state.Guides.map((Guide,index) => (
+                                        <Grid  item xs={12}  key={index}>
+                                            <hr/>
+                                            {this.card(Guide.data())}
+                                        </Grid >
+                                    ))
 
-                            <button onClick={this.handleSubmit} >רשימת מדריכים</button>
-                            <div></div>
-                            <button id="feedback-button" className="btn btn-info" onClick={()=>{BackPage(this.props,this.state.user)}}>חזרה לתפריט</button>
+
+                            }
                         </div>
 
                     </Grid>
+
+                    <Grid item xs={12}>
+                        <div className="text-below-image">
+                            <button onClick={()=>{
+                                this.getAllUsers('students')
+                                this.setState({showStudents:!this.state.showStudents})
+                            }} >{this.state.showStudents?'הסתר רשימת חניכים':'הצג רשימת חניכים'}</button>
+                            {
+                                (this.state.showStudents && this.state.Students )?<div> נמצאו: {this.state.Students.length} חניכים </div>:<div></div>
+                            }
+                            {
+                                (!this.state.Students || !this.state.showStudents)?<div></div>:
+                                    this.state.Students.map((Student,index) => (
+                                        <Grid  item xs={12}  key={index}>
+                                            <hr/>
+                                            {this.card(Student.data())}
+                                        </Grid >
+                                    ))
+
+
+                            }
+                        </div>
+                    <Grid item xs={12}>
+                        <button id="feedback-button" className="btn btn-info" onClick={()=>{BackPage(this.props,this.state.user)}}>חזרה לתפריט</button>
+                    </Grid>
+                    </Grid>
+
+
                 </Grid>
+
+
+
             </div>
 
         )
     }
+
+
+    async getAllUsers(user)
+    {
+        if((user === 'guides' && this.state.Guides)||(user === 'students' && this.state.Students))
+            return
+        var allUsers = []
+        await db.collection(user).get().then(res=>{
+            res.forEach(res=>{
+                if(res.data().uid)
+                    allUsers.push(res)
+            })
+        })
+        if(user === 'guides')
+            this.setState({Guides:allUsers})
+        else if(user === 'students')
+            this.setState({Students:allUsers})
+
+    }
+
 
     async handleSubmit(event)
     {
@@ -156,52 +224,82 @@ class UpdatesFirebase extends Component {
         }
     }
 
-    attendReport() {
 
+    card(user)
+    {
+        console.log(user)
+        // {
+        //     approve: false
+        //     email: "r@gmail.com"
+        //     fname: "r"
+        //     lname: "r"
+        //     phone: "123123"
+        //     team: nv {d_: ji, w_: $r, m_: null, firestore: ov, ff: Vd}
+        //     teamName: "קבוצה 1"
+        //     type: "guides"
+        // }
         return(
-            <div id="instactorReport" className="sec-design" dir='rtl'>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <input type="text" name="team" placeholder="שם קבוצה חדשה" onChange={this.handleChange}/>
-                    </Grid>
-                    <Grid item xs={6} >
-                        <button onClick={()=>{CreateNewTeam(this.state.teamName)}}>צור קבוצה חדשה</button>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <button onClick={()=>{
-                        if(this.state.teamName && this.state.teamName.length > 0)
-                            this.setState({replaceTeamName:true})
-                        else
-                            alert("שם הקבוצה החדשה לא יכול להיות ריק")
-                        }}>החלף שם לקבוצה קיימת </button>
-                    </Grid>
-                    <Grid item xs={8} hidden={!this.state.replaceTeamName}>
-                    <Select  placeholder={" בחר קבוצה להחלפת שם "} options={options} onChange={(e)=>{
-                            console.log(e.label,e.value);
-                            this.setState({teamPath:e.value})
-                        }} required/>
-
-                    </Grid>
-                    <Grid item xs={4} hidden={!this.state.replaceTeamName} >
-                        <button onClick={()=>{
-                            console.log("בוצעה החלפה")
-                            this.setState({replaceTeamName:false})
-                        }}>אישור החלפה</button>
-                    </Grid>
-                        <Grid item xs={12}>
-                            <div className="text-below-image">
-
-                                <button onClick={this.handleSubmit} >רשימת מדריכים</button>
-                                <div></div>
-                                <button id="feedback-button" className="btn btn-info" onClick={()=>{BackPage(this.props,this.state.user)}}>חזרה לתפריט</button>
-                            </div>
-
-                        </Grid>
-                </Grid>
+            <div id="name-group" className="form-group" dir="rtl">
+                <div className="report" id="report">
+                    <div>
+                        <h4> שם: {user.fname+' '+ user.lname} </h4>
+                        <h4> טלפון: {user.phone}</h4>
+                        <h4> אימייל: {user.email}</h4>
+                        <h4> קבוצה: {user.teamName}</h4>
+                    </div>
+                </div>
             </div>
 
         )
     }
+
+
+    // attendReport() {
+    //
+    //     return(
+    //         <div id="instactorReport" className="sec-design" dir='rtl'>
+    //             <Grid container spacing={2}>
+    //                 <Grid item xs={12}>
+    //                     <input type="text" name="team" placeholder="שם קבוצה חדשה" onChange={this.handleChange}/>
+    //                 </Grid>
+    //                 <Grid item xs={6} >
+    //                     <button onClick={()=>{CreateNewTeam(this.state.teamName)}}>צור קבוצה חדשה</button>
+    //                 </Grid>
+    //                 <Grid item xs={6}>
+    //                     <button onClick={()=>{
+    //                     if(this.state.teamName && this.state.teamName.length > 0)
+    //                         this.setState({replaceTeamName:true})
+    //                     else
+    //                         alert("שם הקבוצה החדשה לא יכול להיות ריק")
+    //                     }}>החלף שם לקבוצה קיימת </button>
+    //                 </Grid>
+    //                 <Grid item xs={8} hidden={!this.state.replaceTeamName}>
+    //                 <Select  placeholder={" בחר קבוצה להחלפת שם "} options={options} onChange={(e)=>{
+    //                         console.log(e.label,e.value);
+    //                         this.setState({teamPath:e.value})
+    //                     }} required/>
+    //
+    //                 </Grid>
+    //                 <Grid item xs={4} hidden={!this.state.replaceTeamName} >
+    //                     <button onClick={()=>{
+    //                         console.log("בוצעה החלפה")
+    //                         this.setState({replaceTeamName:false})
+    //                     }}>אישור החלפה</button>
+    //                 </Grid>
+    //                     <Grid item xs={12}>
+    //                         <div className="text-below-image">
+    //
+    //                             <button onClick={this.handleSubmit} >רשימת מדריכים</button>
+    //                             <div></div>
+    //                             <button id="feedback-button" className="btn btn-info" onClick={()=>{BackPage(this.props,this.state.user)}}>חזרה לתפריט</button>
+    //                         </div>
+    //
+    //                     </Grid>
+    //             </Grid>
+    //         </div>
+    //
+    //     )
+    // }
 }
 
 
